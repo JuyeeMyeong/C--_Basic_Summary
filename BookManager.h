9e8ebd73fa0964b5dca30ed47a613f4643c91843
Juyee Myeong <juyee.ming@gmail.com>
2025-06-05 18:03:05 +0900

    complete .h files

--- BookManager.h
+++ BookManager.h
@@ -1,3 +1,4 @@
+#pragma once
 #include <vector>
 #include <string>
 
@@ -8,10 +9,16 @@ class BookManager {
 private:
     vector<Book> books; // 책 목록 저장
 
+    Book* findBookByTitle (const string& title);
+    Book* findBookByAuthor (const string& author);
+
 public:
     // 책 추가 메서드
     void addBook(const string& title, const string& author);
 
     // 모든 책 출력 메서드
     void displayAllBooks() const;
+
+    Book* getBookByTitle (const string& title);
+    Book* getBookByAuthor (const string& author);
 };
