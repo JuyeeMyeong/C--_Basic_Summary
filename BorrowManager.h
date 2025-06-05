7a6ee5a6fe739d902e636779b4664fa41c3a8fe5
Juyee Myeong <juyee.ming@gmail.com>
2025-06-05 18:02:19 +0900

    rename files

---
+++ BorrowManager.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <string>
+#include <unordered_map>
+
+#include "Book.h"
+
+using namespace std;
+class BorrowManager
+{
+private:
+    unordered_map<string, int> stock;
+
+public:
+    void initializeStock (const Book& book, int quality = 3);
+
+    void borrowBook (const string& title);
+
+    void returnBook (const string& title);
+
+    void displayStock() const;
+};
